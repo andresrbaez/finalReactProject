d69cf149fed457d02f443e90c3b26906ca03d4e1
andresrbaez <andres.baez24@hotmail.com>
2022-07-29 20:29:54 -0500

    añadiendo detalles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,66 +1,118 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { filterProductThunk, getProductsThunk } from "../store/slices/products.slice";
+import {
+  filterCategoryThunk,
+  filterProductThunk,
+  getProductsThunk,
+} from "../store/slices/products.slice";
 import { useNavigate } from "react-router-dom";
-import { Card, Col, Row, Button, Form, InputGroup } from 'react-bootstrap'
+import {
+  Card,
+  Col,
+  Row,
+  Button,
+  Form,
+  InputGroup,
+  ListGroup,
+  Accordion
+} from "react-bootstrap";
+import axios from "axios";
+
+
+
 
 const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [ searchValue, setSearchValue ] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [categories, setCategories] = useState([]);
 
-//   const submit = () => {
-//     dispatch(filterProductThunk(searchValue))
-//   }
+  //   const submit = () => {
+  //     dispatch(filterProductThunk(searchValue))
+  //   }
 
   const products = useSelector((state) => state.products);
 
   useEffect(() => {
     dispatch(getProductsThunk());
-    dispatch(filterProductThunk())
-  }, []);
+    dispatch(filterProductThunk());
 
-//   console.log(products);
+    axios
+      .get(
+        "https://ecommerce-api-react.herokuapp.com/api/v1/products/categories"
+      )
+      .then((res) => setCategories(res.data.data.categories));
+  }, []);
 
   return (
     <div>
-      <h1>Home</h1>
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="What are you looking for?"
-          aria-label="What are you looking for?"
-          aria-describedby="basic-addon2"
-          onChange={e => setSearchValue(e.target.value)}
-          value={searchValue}
-        />
-        <Button 
-        variant="outline-dark" 
-        onClick={() => dispatch(filterProductThunk(searchValue))}
-        id="button-addon1"
-        style={{background: '#2c3e50', color: '#fff'}}
-        >
-          Search
-        </Button>
-      </InputGroup>
-      <Row xs={1} md={3} className="g-4" >
-        {products.map((product) => (
-          <Col key={product.id}>
-            <Card onClick={() => navigate(`/products/${product.id}`)} style={{cursor: "pointer", minHeight: "380px"}}>
-              <Card.Img variant="top" src={product.productImgs} className='img-products'/>
-              <Card.Body>
-                <Card.Title>{product.title}</Card.Title>
-                <Card.Text>
-                  Price: ${product.price}
-                </Card.Text>
+      <Row>
+        <Col lg={3}>
+          <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Categories</Accordion.Header>
+              <Accordion.Body>
+                <ListGroup variant="flush">
+                  {categories.map((category) => (
+                    <ListGroup.Item
+                      key={category.id}
+                      onClick={() => dispatch(filterCategoryThunk(category.id))}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {category.name}
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Col>
+
+        <Col>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="What are you looking for?"
+              aria-label="What are you looking for?"
+              aria-describedby="basic-addon2"
+              onChange={(e) => setSearchValue(e.target.value)}
+              value={searchValue}
+            />
+            <Button
+              variant="outline-dark"
+              onClick={() => dispatch(filterProductThunk(searchValue))}
+              id="button-addon1"
+              style={{ background: "#2c3e50", color: "#fff"}}
+            >
+              Search
+            </Button>
+          </InputGroup>
+          <Row xs={1} md={3} className="g-4">
+            {products.map((product) => (
+              <Col key={product.id}>
+                <Card
+                  onClick={() => navigate(`/products/${product.id}`)}
+                  style={{ cursor: "pointer", minHeight: "360px"}}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={product.productImgs}
+                    className="img-products"
+                  />
+                  <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>Price: ${product.price}</Card.Text>
                     <button className="add-cart-btn">
-                        <i className='bx bxs-cart icon-nav' 
-                        style={{color:'#ffffff'}}
-                        ></i>
+                      <i
+                        className="bx bxs-cart icon-nav"
+                        style={{ color: "#ffffff" }}
+                      ></i>
                     </button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
       </Row>
     </div>
   );
--- src/store/slices/products.slice.jsx
+++ src/store/slices/products.slice.jsx
@@ -27,6 +27,13 @@ export const filterProductThunk = (searchValue) => (dispatch) => {
         .finally(() => dispatch(setIsLoading(false)));
 }
 
+export const filterCategoryThunk = (categoryId) => (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios.get(`https://ecommerce-api-react.herokuapp.com/api/v1/products?category=${categoryId}`)
+        .then((res) => dispatch(setProducts(res.data.data.products)))
+        .finally(() => dispatch(setIsLoading(false)));
+}
+
 export const { setProducts } = productsSlice.actions;
 
 export default productsSlice.reducer;
