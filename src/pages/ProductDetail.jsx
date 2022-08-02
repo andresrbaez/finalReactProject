e0f97ac1bffee7f50074aa3551d5eed9ef7ac565
andresrbaez <andres.baez24@hotmail.com>
2022-08-02 12:01:07 -0500

    actualizando

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsThunk } from "../store/slices/products.slice";
-import { Carousel, Col, Row } from "react-bootstrap";
+import { Card, Carousel, Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   const allProducts = useSelector((state) => state.products);
   const [productDetail, setProductDetail] = useState({});
-  const [ suggestedProducts, setSuggestedProducts ] = useState([])
+  const [suggestedProducts, setSuggestedProducts] = useState([]);
 
   const { id } = useParams();
 
@@ -19,16 +19,17 @@ const ProductDetail = () => {
     );
     setProductDetail(productsFound);
 
-    const filteredProducts = allProducts.filter(productItem => productItem.category.id === productsFound.category.id)
-    setSuggestedProducts(suggestedProducts)
-    
+    const filteredProducts = allProducts.filter(
+      (productItem) => productItem.category.id === productsFound.category.id
+    );
+    setSuggestedProducts(filteredProducts);
   }, [allProducts]);
 
   useEffect(() => {
     dispatch(getProductsThunk());
   }, []);
 
-  console.log(productDetail);
+  // console.log(productDetail);
 
   return (
     <div>
@@ -52,7 +53,6 @@ const ProductDetail = () => {
                 className="d-block w-100"
                 src={productDetail?.productImgs?.[1]}
                 style={{ height: "400px", objectFit: "contain" }}
-
                 alt="Second slide"
               />
             </Carousel.Item>
@@ -67,20 +67,54 @@ const ProductDetail = () => {
           </Carousel>
         </Col>
         <Col>
-            <h1>{productDetail?.title}</h1>
-            <strong>Price: </strong>{productDetail?.price}
-            <p>{productDetail?.description}</p>
-            <ul>
-              {
-                suggestedProducts.map(products => (
-                  <div>
-                    {products.title}
-                  </div>
-                ))
-              }
-            </ul>
+          <h1>{productDetail?.title}</h1>
+          <strong>Price: </strong>
+          {productDetail?.price}
+          <h4>Description</h4>
+          <p>{productDetail?.description}</p>
         </Col>
       </Row>
+      <div>
+        <div>
+          <h4>Suggested Products</h4>
+        </div>
+        <div>
+        <Row xs={1} md={3} className="g-4">
+            {suggestedProducts.map((product) => (
+              <Col key={product.id}>
+                <Card
+                  onClick={() => navigate(`/products/${product.id}`)}
+                  style={{ cursor: "pointer", minHeight: "360px"}}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={product.productImgs}
+                    className="img-products"
+                  />
+                  <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>Price: ${product.price}</Card.Text>
+                    <button className="add-cart-btn">
+                      <i
+                        className="bx bxs-cart icon-nav"
+                        style={{ color: "#ffffff" }}
+                      ></i>
+                    </button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          {/* <ul className="ul-suggested">
+            {suggestedProducts.map((products) => (
+              <div key={products.id} className="suggested-display">
+                <img className="img-suggested" src={products.productImgs} alt="" />
+                <p>{products.title}</p>
+              </div>
+            ))}
+          </ul> */}
+        </div>
+      </div>
     </div>
   );
 };
