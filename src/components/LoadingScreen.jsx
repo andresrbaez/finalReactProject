2de45e2a0256e13d5a9edfad031578236af06894
andresrbaez <andres.baez24@hotmail.com>
2022-08-10 15:20:53 -0500

    actualizacion

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
-import '../styles/loadingScreen.css';
+import React, { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+import "../styles/loadingScreen.css";
+import { ClipLoader, HashLoader } from "react-spinners";
 
 const LoadingScreen = () => {
-    return (
-        <div className='overlay'>
-            <Spinner animation="grow" variant="dark"/>
-            Loading...
-            
-        </div>
-    );
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
+
+  return (
+    <div className="overlay">
+      <HashLoader color="#2c3e50" size={130} loading={loading}/>
+      {/* <Spinner animation="grow" variant="dark"/>
+            Loading... */}
+    </div>
+  );
 };
 
 export default LoadingScreen;
